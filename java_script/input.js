6d0a6498540f416429ed6f6b3905e9aeff71bf14
Ramon_Trigo <ramtrigo@gmail.com>
2023-02-07 11:03:01 -0300

    input_java

---
+++ java_script/input.js
@@ -0,0 +1,20 @@
+function sumeti(){
+    document.getElementById("log").innerHTML +="submeti<br>";
+    return false;
+}
+function entreiNoCampo(){
+    document.getElementById("log").innerHTML +="entrei no campo<br>";
+}
+function resetei(){
+    document.getElementById("log".innerHTML +="resetei<br>");
+}
+function saidoCampo(){
+    document.getElementById("log").innerHTML +="sai do campo<br>"
+}
+function mudei(){
+    document.getElementById("log").innerHTML +="mudei<br>";
+}
+function selecionei()
+{
+    document.getElementById("log").innerHTML += "selecionei<br>";
+}
